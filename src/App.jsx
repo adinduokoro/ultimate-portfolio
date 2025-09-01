f87d8a83e0d8f7c819449cd1cee68f55aeb6d117
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2025-09-01 00:35:44 -0400

    Implement base routing and install dependencies
    Updated package.json and package-lock.json with new dependencies (gsap and react-router-dom).
    Modified src/App.jsx to set up basic application structure.
    Modified src/pages/Home.jsx (which was previously untracked) to create the home page component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages";
 
 function App() {
-  return <div className="app">
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { HeroCarousel } from "../components";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <HeroCarousel />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
